5bbd6b675d0d9d0585dedb5829bf6307d2c8a0c4
Bruno <>
2019-07-19 21:23:22 -0300

    mudei alguns arquivos

--- application/views/Login.php
+++ application/views/Login.php
@@ -3,7 +3,7 @@
     <!--Responsivo para smartphone, tablet e PC-->
     <div class="col-xs-1 col-sm-1 col-lg-3"></div>
     <div class="col-xs-10 col-sm-10 col-lg-6">
-        <img src="pc.jpg" class="img-fluid" alt="Responsive image">
+        <img src="pc..jpg" class="img-fluid" alt="Responsive image">
         <h2>Login</h2>
         <?php echo form_open('login/autenticar'); ?>
         <div class="form-group">
